154b14f956a06626d6906031656d980f813e3cac
afrobug <afrojahmed6@gmail.com>
2023-05-03 00:29:55 +0530

    changesdone

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,43 @@
-import React from 'react'
+import React from "react";
 
-import '../styles/App.css';
+import "../styles/App.css";
 
 const data = {
-  '2018': [
-    'Avengers 1',
-    'Now you see me',
-    'Fast and Furious 7'
+  2018: ["Avengers 1", "Now you see me", "Fast and Furious 7"],
+  2019: ["Avengers 2", "Now you see me 2", "Fast and Furious 8"],
+  2020: [
+    "Final Avengers Movie(Iron man dies)",
+    "Now you finally used Lenskart",
+    "Covid Came",
   ],
-  '2019': [
-    'Avengers 2',
-    'Now you see me 2',
-    'Fast and Furious 8'
-  ],
-  '2020': [
-    'Final Avengers Movie(Iron man dies)',
-    'Now you finally used Lenskart',
-    'Covid Came'
-  ],
-  '2021': [
-    'Covid Returns',
-  ],
-  '2022': [
-    'Adventures of Saiman',
-    'Adventures of Shaktiman'
-  ]
-}
-const App = () => {
+  2021: ["Covid Returns"],
+  2022: ["Adventures of Saiman", "Adventures of Shaktiman"],
+};
 
+const years = Object.keys(data);
+console.log(years);
+
+const App = () => {
+  const [value, setValue] = React.useState("") 
+
+  const handleDropdown = (event)=>{
+    // console.log("msg", event.target.value)
+    setValue(event.target.value)
+  }
   return (
     <div id="main">
-      
-    </div>
-  )
-}
+      <select onChange={handleDropdown}>
+        <option value="">Null</option>
+        {
+          years.map((year)=>{
+            return <option value={year}>{year}</option>
 
+          })
+        }
+      </select>
+      <div>{data[value]}</div>
+    </div>
+  );
+};
 
 export default App;
